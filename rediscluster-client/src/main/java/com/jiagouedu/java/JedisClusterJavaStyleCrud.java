ef5d22403796d46fa0bdbefd17e901e59ca9be5c
592306832@qq.com <592306832@qq.com>
2018-12-23 20:54:52 +0800

    tlcache

--- rediscluster-client/src/main/java/com/jiagouedu/java/JedisClusterJavaStyleCrud.java
+++ rediscluster-client/src/main/java/com/jiagouedu/java/JedisClusterJavaStyleCrud.java
@@ -1,21 +1,4 @@
-package com.jiagouedu.java;/*
- * ━━━━━━如来保佑━━━━━━
- * 　　　┏┓　　　┏┓
- * 　　┏┛┻━━━┛┻┓
- * 　　┃　　　━　　　┃
- * 　　┃　┳┛　┗┳　┃
- * 　　┃　　　┻　　　┃
- * 　　┗━┓　　　┏━┛
- * 　　　　┃　　　┗━━━┓
- * 　　　　┃　　　　　　　┣┓
- * 　　　　┃　　　　　　　┏┛
- * 　　　　┗┓┓┏━┳┓┏┛
- * 　　　　　┗┻┛　┗┻┛
- * ━━━━━━永无BUG━━━━━━
- * 图灵学院-悟空老师
- * www.jiagouedu.com
- * 悟空老师QQ：245553999
- */
+package com.jiagouedu.java;
 
 
 import org.junit.Test;
